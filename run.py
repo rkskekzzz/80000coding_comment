aae0e49e6eba37ae1828125874d0b8c8314a47f7
rkskekzzz <wkdlfflxh@naver.com>
2022-07-22 14:12:23 +0900

    hotfix

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 import requests
 import json
 
-COMMENT_URL = os.getenv('COMMENT_URL')
-HOST_URL = os.getenv('HOST_URL')
+COMMENT_URL = os.environ['COMMENT_URL']
+HOST_URL = os.environ['HOST_URL']
 
 response = requests.get(COMMENT_URL)
 responseJson = json.loads(response.text)
@@ -13,18 +13,12 @@ BODY = responseJson[-1]['body']
 SENDER =  responseJson[-1]['user']['login']
 DATABASE_ID = HOST_URL.split("/")[-1].replace("-","")
 
-print(BODY)
-print(SENDER)
-print(DATABASE_ID)
-
 headers = {
-    "Authorization": f"Bearer " + f"{os.getenv('BEARER')}",
+    "Authorization": f"Bearer {os.environ['BEARER']}",
     "Accept": "application/json",
     "Notion-Version": "2022-02-22",
     "Content-Type": "application/json"
 }
-
-print(headers)
 
 def updateComment():
     _apiUrl = "https://api.notion.com/v1/comments"
@@ -68,7 +62,7 @@ def updateComment():
                 "text": {
                     "content": "from oopy",
                     "link": {
-                        "url" : "https://80000coding.oopy.io/ddcdbb4d-461a-4a64-97b8-5040fc49c7d6"
+                        "url" : "https://80000coding.oopy.io/" + DATABASE_ID
                     }
                 },
                 "annotations": {
